23ecb50625211f30ac054783ea83b334d046c57c
Eben Palazzolo <olozzalap@gmail.com>
2013-05-29 19:24:08 -0400

    game functionality added with bust conditions, helper methods added for calculating total and card images now displayed

--- main.rb
+++ main.rb
@@ -5,30 +5,30 @@ set :sessions, true
 
 helpers do
 
-  def card_display(card)
+  def card_img_selector (card)
     face = case card[0]
       when 2..10 then card[0]
-      when 'J' then 'Jack'
-      when 'Q' then 'Queen'
-      when 'K' then 'King'
-      when 'A' then 'Ace'
+      when 'J' then 'jack'
+      when 'Q' then 'queen'
+      when 'K' then 'king'
+      when 'A' then 'zce'
     end
     suit = case card[1]
-      when 'H' then 'Hearts'
-      when 'D' then 'Diamonds'
-      when 'S' then 'Spades'
-      when 'C' then 'Clubs'
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'S' then 'spades'
+      when 'C' then 'clubs'
     end
-    return "#{face} of #{suit}"
+    return "/img/cards/#{suit}_#{face}.jpg"
   end
   
   def calculate_total (hand)
     total = 0
     aces = 0
-    for i in 0..hand.count
+    for i in 0...hand.count
       if hand[i][0] == 'J' || hand[i][0] == 'Q' || hand[i][0] == 'K'
         total += 10
-      elsif hand[i][0] == 2..10
+      elsif hand[i][0].to_i != 0
         total += hand[i][0]
       elsif hand[i][0] == 'A'
         total += 11
@@ -40,8 +40,18 @@ helpers do
       total -= 10
     end
     return total
-  end
+  end 
   
+  def dealer_action?
+    if calculate_total(session[:dealer_cards]) > 21
+      @dealer_bust = true
+    elsif 
+  end
+
+end
+
+before do
+  @show_hit_and_stay_buttons = true
 end
 
 
@@ -79,8 +89,17 @@ get '/game' do
   erb :game
 end
 
-post '/hit_stay' do
-  if params[:hit]
-    session[:player_cards] << session[:deck].pop
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > 21
+    @error = "You have busted, the dealer wins!"
+    @show_hit_and_stay_buttons = false
   end
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "You have chosen to stay"
+  @show_hit_and_stay_buttons = false
+  erb :game
 end
