23e512a524aff918227b9f6cc1c530c9cbb8df54
Eben Palazzolo <olozzalap@gmail.com>
2013-05-31 13:18:39 -0400

    fully functional gameplay now with dealer hit and stay and winning functionality

--- main.rb
+++ main.rb
@@ -11,7 +11,7 @@ helpers do
       when 'J' then 'jack'
       when 'Q' then 'queen'
       when 'K' then 'king'
-      when 'A' then 'zce'
+      when 'A' then 'ace'
     end
     suit = case card[1]
       when 'H' then 'hearts'
@@ -45,7 +45,18 @@ helpers do
   def dealer_action?
     if calculate_total(session[:dealer_cards]) > 21
       @dealer_bust = true
-    elsif 
+      @show_cards = true
+      @success = "The dealer has busted and you win!"
+    elsif calculate_total(session[:dealer_cards]) == 21
+      @show_cards = true
+      @error = "Sorry the dealer has Blackjack and you lose!"
+    elsif calculate_total(session[:dealer_cards]) > 16
+      @dealer_stay = true
+      redirect '/game/compare'
+    else
+      @dealer_hit = true
+      erb :game
+    end
   end
 
 end
@@ -65,8 +76,9 @@ end
 
 post '/new_user' do
   session[:username] = params[:username]
-  if session[:username] == nil
-  	erb :new_user
+  if params[:username].empty?
+    @error = "A username is required, hotshot!"
+    halt erb :new_user
   else
   	redirect '/game'
   end
@@ -91,7 +103,10 @@ end
 
 post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
-  if calculate_total(session[:player_cards]) > 21
+  if calculate_total(session[:player_cards]) == 21
+    @success = "Congratulations, you have 21 and you win!"
+    @show_hit_and_stay_buttons = false
+  elsif calculate_total(session[:player_cards]) > 21
     @error = "You have busted, the dealer wins!"
     @show_hit_and_stay_buttons = false
   end
@@ -101,5 +116,28 @@ end
 post '/game/player/stay' do
   @success = "You have chosen to stay"
   @show_hit_and_stay_buttons = false
+  redirect '/game/dealer'
+end
+
+get '/game/dealer' do
+  @show_hit_and_stay_buttons = false
+  dealer_action?
+  erb :game
+end
+
+get '/game/dealer/hit' do
+  @show_hit_and_stay_buttons = false
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
+end
+
+get '/game/compare' do
+  @show_cards = true
+  @show_hit_and_stay_buttons = false
+  if calculate_total(session[:player_cards]) > calculate_total(session[:dealer_cards])
+    @success = "The dealer has #{calculate_total(session[:dealer_cards])} and you have #{calculate_total(session[:player_cards])}. You win!!!"
+  else
+    @error = "The dealer has #{calculate_total(session[:dealer_cards])} and you have #{calculate_total(session[:player_cards])}. You lose..."
+  end
   erb :game
 end
